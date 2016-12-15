f65f644bf3ed094c3d227d99451e6afd765d999f
DennisHub <dennis.huben@rwth-aachen.de>
2016-12-15 12:17:17 +0100

    Add Links for different species to show table of individuals

--- database_mockup/src/sample/ConnectionManager.java
+++ database_mockup/src/sample/ConnectionManager.java
@@ -72,6 +72,56 @@ public class ConnectionManager {
         }
     }
 
+    public void tableFromDatabase(GridPane grid, String select_what, String select_from, String select_condition) {
+        ArrayList columnNames = new ArrayList();
+        ArrayList data = new ArrayList();
+
+        //  Connect to an MySQL Database, run query, get result set
+        String sql = "SELECT " + select_what + " FROM " + select_from + " WHERE " + select_condition;
+
+        try (Connection connection = DriverManager.getConnection(url, userid, password);
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            ResultSetMetaData md = rs.getMetaData();
+            int columns = md.getColumnCount();
+
+            //  Get column names
+            for (int i = 1; i <= columns; i++) {
+                columnNames.add(md.getColumnName(i));
+            }
+            while (rs.next()) {
+                ArrayList row = new ArrayList(columns);
+
+                for (int i = 1; i <= columns; i++) {
+                    row.add(rs.getObject(i));
+                }
+
+                data.add(row);
+            }
+
+
+            for (int i = 0; i < columns; i++) {
+                Text heading = new Text((String) columnNames.get(i));
+                grid.add(heading, i+2, 2);
+
+            }
+
+            for (int i = 0; i < data.size(); i++){
+                for (int j = 0; j < columns; j++){
+                    ArrayList currentRow = (ArrayList) data.get(i);
+                    Text buff = new Text((String) currentRow.get(j).toString());
+                    grid.add(buff, j+2, i+3);
+                }
+
+            }
+
+        }
+        catch (SQLException e)
+        {
+            System.out.println( e.getMessage() );
+        }
+    }
+
     public Long getCount(String count_what, String count_from){
         String sql = "SELECT COUNT(DISTINCT " + count_what +") FROM " + count_from;
 
@@ -107,6 +157,27 @@ public class ConnectionManager {
         }
         Long zero = new Long(0);
         return zero;
+    }
+
+    public ArrayList<String> getSpecies(String count_what, String count_from, String count_condition){
+        String sql = "SELECT distinct " + count_what +" FROM " + count_from + " WHERE " + count_condition;
+        ArrayList<String> species_names = new ArrayList<String>();
+
+        try (Connection connection = DriverManager.getConnection(url, userid, password);
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while(rs.next()) {
+                species_names.add((String) rs.getObject(1));
+
+            }
+            return species_names;
+        }
+        catch (SQLException e)
+        {
+            System.out.println( e.getMessage() );
+        }
+
+        return species_names;
     }
 
 }
