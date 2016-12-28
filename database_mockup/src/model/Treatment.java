057d0f85666cc9eda61bd7bdc896748496f2f079
Jan Grewe <jan.grewe@g-node.org>
2016-12-28 22:02:24 +0100

    [hibernate] add Treatment mappings and classes

---
+++ database_mockup/src/model/Treatment.java
@@ -0,0 +1,84 @@
+package model;
+
+import java.util.Date;
+
+/**
+ * Created by jan on 28.12.16.
+ */
+public class Treatment {
+    private Long id;
+    private Date start;
+    private Date end;
+    private Subject subject;
+    private Person person;
+    private TreatmentType type;
+
+    public Treatment() {}
+
+    public Treatment(Date start, Subject subject, Person person, TreatmentType type) {
+        this.start = start;
+        this.subject = subject;
+        this.person = person;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Subject subject) {
+        this.subject = subject;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public TreatmentType getType() {
+        return type;
+    }
+
+    public void setType(TreatmentType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Treatment{" +
+                "id=" + id +
+                ", type=" + type +
+                ", start=" + start +
+                ", end=" + ((end != null) ? end : "") +
+                ", subject=" + subject +
+                ", person=" + person +
+                '}';
+    }
+}
---
+++ database_mockup/src/model/TreatmentType.java
@@ -0,0 +1,71 @@
+package model;
+
+/**
+ * Created by jan on 28.12.16.
+ */
+public class TreatmentType {
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean invasive;
+    private License license;
+
+    public TreatmentType() {}
+
+    public TreatmentType(Long id, String name, Boolean invasive) {
+        this.id = id;
+        this.name = name;
+        this.invasive = invasive;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean isInvasive() {
+        return invasive;
+    }
+
+    public void setInvasive(Boolean invasive) {
+        this.invasive = invasive;
+    }
+
+    public License getLicense() {
+        return license;
+    }
+
+    public void setLicense(License license) {
+        this.license = license;
+    }
+
+    @Override
+    public String toString() {
+        return "TreatmentType{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", invasive=" + invasive +
+                ", license=" + ((license != null) ? license.getName() : "") +
+                '}';
+    }
+}
