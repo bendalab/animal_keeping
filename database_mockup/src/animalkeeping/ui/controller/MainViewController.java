ef70cb6cfaf57cad34ed9af8a04176c285f6a4b6
Jan Grewe <jan.grewe@g-node.org>
2017-01-01 17:04:04 +0100

    [MainView] add a few buttons

--- database_mockup/src/animalkeeping/ui/controller/MainViewController.java
+++ database_mockup/src/animalkeeping/ui/controller/MainViewController.java
@@ -1,29 +1,14 @@
 package animalkeeping.ui.controller;
 
-import animalkeeping.ui.Main;
+import animalkeeping.ui.InventoryTable;
+import animalkeeping.ui.PersonsTable;
 import animalkeeping.ui.SubjectsTable;
-import animalkeeping.ui.Main;
-import animalkeeping.ui.SubjectsTable;
+import animalkeeping.ui.TreatmentsTable;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
 
 public class MainViewController {
-    private AnchorPane personView = null;
-
-    public MainViewController() {
-        FXMLLoader loader = new FXMLLoader();
-        try {
-            System.out.println(Main.class.getResource("fxml/PersonsView.fxml"));
-
-            loader.setLocation(Main.class.getResource("fxml/PersonsView.fxml"));
-            this.personView = (AnchorPane) loader.load();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     @FXML
     private Button personsBtn;
@@ -35,26 +20,36 @@ public class MainViewController {
     private Button subjectsBtn;
 
     @FXML
-    private Button housingBtn;
+    private Button homeBtn;
 
     @FXML
     private ScrollPane scrollPane;
 
     @FXML
     private void showPersons() {
-        System.out.println("show Persons!");
         this.scrollPane.setContent(null);
-        this.scrollPane.setContent(personView);
+        PersonsTable personTable = new PersonsTable();
+        this.scrollPane.setContent(personTable);
     }
 
     @FXML
     private void showSubjects() {
-        System.out.println("show subjects!");
         this.scrollPane.setContent(null);
         SubjectsTable subjectsTable = new SubjectsTable();
-
         this.scrollPane.setContent(subjectsTable);
     }
 
+    @FXML
+    private void showTreatments() {
+        this.scrollPane.setContent(null);
+        TreatmentsTable treatmentsTable = new TreatmentsTable();
+        this.scrollPane.setContent(treatmentsTable);
+    }
 
+    @FXML
+    private void showInventory() {
+        this.scrollPane.setContent(null);
+        InventoryTable inventoryTable= new InventoryTable();
+        this.scrollPane.setContent(inventoryTable);
+    }
 }
