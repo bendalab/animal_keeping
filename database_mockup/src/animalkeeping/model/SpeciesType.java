a0c3d01ade27e2b8e90f63d8a045d2df23f99d64
Jan Grewe <jan.grewe@g-node.org>
2017-01-14 21:00:21 +0100

    [SpeciesType] fix count query

--- database_mockup/src/animalkeeping/model/SpeciesType.java
+++ database_mockup/src/animalkeeping/model/SpeciesType.java
@@ -47,14 +47,7 @@ public class SpeciesType {
         this.trivial = trivial;
     }
 
-    /*
-        I have not yet found a way to reliably store this information. Problem is, it initializes with the wrong values.
-     */
     public Integer getCount() {
-        if(true)
-        return getCountInit();
-
-        else
         return count;
     }
 
@@ -62,37 +55,6 @@ public class SpeciesType {
         this.count = count;
     }
 
-    /* Go through all subjects; for each subject, check if any housing associated with that fish has no
-    end date; if true, that means the fish is still in one of the housing units. In this case, it is assumed that
-    the fish is alive.
-     */
-    public Integer getCountInit() {
-        Session session = Main.sessionFactory.openSession();
-        int numberAlive = 0;
-        try {
-            session.beginTransaction();
-            List<Subject> result = session.createQuery("from Subject where species_id = " + this.getId().toString()).list();
-
-            for (Subject currentSubject : result) {
-                Long index = currentSubject.getId();
-                List<Housing> houseList = session.createQuery("from Housing where subject_id = " + index.toString()).list();
-                for (Housing house : houseList) {
-                    if (house.getEnd() == null) {
-                        numberAlive += 1;
-                        break;
-                    }
-                }
-            }
-            session.getTransaction().commit();
-            session.close();
-        } catch (HibernateException e) {
-            e.printStackTrace();
-            if (session.isOpen()) {
-                session.close();
-            }
-        }
-        this.count = numberAlive;
-        return  numberAlive;
     }
 
 
