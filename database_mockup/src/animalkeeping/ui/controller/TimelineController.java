b815e09a0e305b7255f86bf293df2e445de73012
Jan Grewe <jan.grewe@g-node.org>
2017-01-17 18:26:04 +0100

    [Timeline] add plot for timelines of treatments

---
+++ database_mockup/src/animalkeeping/ui/controller/TimelineController.java
@@ -0,0 +1,93 @@
+package animalkeeping.ui.controller;
+
+
+import animalkeeping.model.Treatment;
+import animalkeeping.model.TreatmentType;
+import animalkeeping.ui.DateAxis;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Side;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+
+public class TimelineController extends VBox implements Initializable {
+
+    @FXML private AnchorPane plotPane;
+    private ScatterChart<Date, Number> timelineChart;
+    private DateAxis xAxis;
+    private NumberAxis yAxis;
+
+    public TimelineController() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/TimelinePlot.fxml"));
+        loader.setController(this);
+        try {
+            this.getChildren().add(loader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public TimelineController(Set<Treatment> treatments) {
+        this();
+        setTreatments(treatments);
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("initialize");
+        xAxis = new DateAxis(new Date(0), new Date());
+        yAxis = new NumberAxis(0, 10, 5);
+        timelineChart = new ScatterChart<Date, Number>(xAxis, yAxis);
+        //timelineChart.re
+        timelineChart.autosize();
+        timelineChart.setPrefHeight(150);
+        timelineChart.setPrefWidth(1000);
+        timelineChart.setLegendSide(Side.RIGHT);
+        plotPane.getChildren().clear();
+        plotPane.getChildren().add(timelineChart);
+    }
+
+
+    public void setTreatments(Set<Treatment> treatments) {
+        if (treatments == null) {
+            timelineChart.setData(null);
+            return;
+        }
+        timelineChart.getXAxis().setAutoRanging(true);
+        timelineChart.getYAxis().setAutoRanging(true);
+        timelineChart.getYAxis().setTickLabelsVisible(false);
+
+        ObservableList<XYChart.Series<Date, Number>> seriesData = FXCollections.observableArrayList();
+        HashMap<String, ObservableList<XYChart.Data<Date, Number>>> series = new HashMap<>();
+        HashMap<String, Integer> seriesNumber = new HashMap<>();
+        for (Treatment t : treatments) {
+            if (!series.containsKey(t.getType().getName())) {
+                ObservableList<XYChart.Data<Date, Number>> temp = FXCollections.observableArrayList();
+                seriesNumber.put(t.getType().getName(), seriesNumber.size() + 1);
+                temp.add(new XYChart.Data<Date, Number>(t.getStart(), seriesNumber.get(t.getType().getName())));
+                series.put(t.getType().getName(), temp);
+            } else {
+                ObservableList<XYChart.Data<Date, Number>> temp = series.get(t.getType().getName());
+                temp.add(new XYChart.Data<Date, Number>(t.getStart(), seriesNumber.get(t.getType().getName())));
+            }
+        }
+        for (String s : series.keySet()) {
+            seriesData.add(new XYChart.Series<>(s, series.get(s)));
+        }
+        timelineChart.setData(seriesData);
+    }
+
+
+}
