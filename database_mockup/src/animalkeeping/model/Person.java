01bee29a4709f84713d3c79fb5a5e838a1e94972
Jan Grewe <jan.grewe@g-node.org>
2017-01-28 13:05:15 +0100

    [search] rebasing

--- database_mockup/src/animalkeeping/model/Person.java
+++ database_mockup/src/animalkeeping/model/Person.java
@@ -1,7 +1,7 @@
 package animalkeeping.model;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 
 import java.util.HashSet;
 import java.util.Set;
