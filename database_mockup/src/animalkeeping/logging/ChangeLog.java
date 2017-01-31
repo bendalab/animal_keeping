49b15e072de85ada0b8d6b839d43470a2ceafbbc
DennisHub <dennis.huben@rwth-aachen.de>
2017-01-31 11:59:40 +0100

    Add ChangeLog to mappings in hibernate.cfg.xml and added setter/getter to ChangeLog

--- database_mockup/src/animalkeeping/logging/ChangeLog.java
+++ database_mockup/src/animalkeeping/logging/ChangeLog.java
@@ -2,6 +2,7 @@ package animalkeeping.logging;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.Date;
 
 /**
  * Created by huben on 30.01.17.
@@ -19,6 +20,46 @@ public class ChangeLog {
         this.typeOfAction = action;
         this.actionTime = Timestamp.from(Instant.now());
         this.targetType = type;
+        this.targetId = targetId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTypeOfAction() {
+        return typeOfAction;
+    }
+
+    private void setTypeOfAction(String typeOfAction) {
+        this.typeOfAction = typeOfAction;
+    }
+
+    public Date geactionTime() {
+        return actionTime;
+    }
+
+    private void setActionTime(Date actionTime) {
+        this.actionTime = actionTime;
+    }
+
+    public String getTargetType() {
+        return targetType;
+    }
+
+    private void setTargetType(String targetType) {
+        this.targetType = targetType;
+    }
+
+    public Long getTargetId() {
+        return targetId;
+    }
+
+    private void setTargetId(Long targetId) {
         this.targetId = targetId;
     }
 }
