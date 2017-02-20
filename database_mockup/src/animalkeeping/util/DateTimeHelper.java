833ebcf8bba66e1e3e49fa24565a138e6213bd90
Jan Grewe <jan.grewe@g-node.org>
2017-02-20 11:54:25 +0100

    [DateTimeHelper] add method to convert to localDate

--- database_mockup/src/animalkeeping/util/DateTimeHelper.java
+++ database_mockup/src/animalkeeping/util/DateTimeHelper.java
@@ -3,6 +3,7 @@ package animalkeeping.util;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 
 public class DateTimeHelper {
@@ -26,6 +27,29 @@ public class DateTimeHelper {
         return datetime;
     }
 
+    public static Date localDateToUtilDate(LocalDate d) {
+        String dstr = d.toString();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date;
+        try {
+            date = dateFormat.parse(dstr);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return date;
+    }
+
+    public static LocalDate toLocalDate(java.sql.Date date) {
+        return ((java.sql.Date) date).toLocalDate();
+    }
+
+    public static LocalDate toLocalDate(java.util.Date date) {
+        if (date instanceof java.sql.Date) {
+            return toLocalDate((java.sql.Date) date);
+        }
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    }
 
     public static Boolean validateTime(String time_str) {
         DateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
