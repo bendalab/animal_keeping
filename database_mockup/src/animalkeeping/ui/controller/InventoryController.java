a295eefda11c2c1a18b8d1041d58d03d99b7715f
Jan Grewe <jan.grewe@g-node.org>
2017-02-24 14:54:33 +0100

    [Inventory] export report on animal use, correct used animal numbers

--- database_mockup/src/animalkeeping/ui/controller/InventoryController.java
+++ database_mockup/src/animalkeeping/ui/controller/InventoryController.java
@@ -1,14 +1,15 @@
 package animalkeeping.ui.controller;
 
 import animalkeeping.model.*;
-import animalkeeping.ui.*;
+import animalkeeping.ui.ControlLabel;
+import animalkeeping.ui.HousingTable;
+import animalkeeping.ui.Main;
+import animalkeeping.ui.View;
 import animalkeeping.util.Dialogs;
 import animalkeeping.util.EntityHelper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -18,11 +19,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tooltip;
 import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.util.Pair;
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -30,13 +29,13 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.function.Predicate;
 
 /**
  * Created by jan on 14.01.17.
@@ -67,8 +66,13 @@ public class InventoryController extends VBox implements Initializable, View {
     public void initialize(URL location, ResourceBundle resources) {
         unitsHashMap = new HashMap<>();
         controls = new VBox();
-        animalUseLabel = new ControlLabel("export animal use", true);
+        animalUseLabel = new ControlLabel("export animal use", false);
         animalUseLabel.setTooltip(new Tooltip("export excel sheet containing the animal use per license"));
+        animalUseLabel.setOnMouseClicked(event -> {
+            if(event.getButton().equals(MouseButton.PRIMARY)) {
+                exportAnimalUse();
+            }
+        });
         exportStock = new ControlLabel("export stock list", false);
         exportStock.setTooltip(new Tooltip("Export current stock list to excel sheet."));
         exportStock.setOnMouseClicked(event -> {
@@ -306,4 +310,135 @@ public class InventoryController extends VBox implements Initializable, View {
             }
         }
     }
+
+
+    private List<License> getLicenseList(Date start, Date end) {
+        String q = "From License where start_date < :end AND end_date > :start";
+        try {
+            Session session = Main.sessionFactory.openSession();
+            Query query = session.createQuery(q, License.class);
+            query.setParameter("start", start);
+            query.setParameter("end", end);
+            session.beginTransaction();
+            List<License> l = query.list();
+            session.getTransaction().commit();
+            session.close();
+            return l;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    private void exportAnimalUse() {
+        Pair<Date, Date> interval = Dialogs.getDateInterval();
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        List<License> licenses = getLicenseList(interval.getKey(), interval.getValue());
+        for (License l : licenses) {
+            String s = l.getNumber();
+            String sheetName = l.getName();
+            XSSFSheet sheet = workbook.createSheet(sheetName);
+            exportLicense(l, sheet, interval.getKey(), interval.getValue());
+        }
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Select output file");
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Excel spreadsheet (*.xlsx)", "*.xlsx"));
+        chooser.setInitialFileName("*.xlsx");
+        File f = chooser.showSaveDialog(Main.getPrimaryStage());
+        if (f != null) {
+            try {
+                FileOutputStream out = new FileOutputStream(f);
+                workbook.write(out);
+                out.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public void exportLicense(License l, XSSFSheet sheet, Date start, Date end) {
+        int rowid = 0;
+        XSSFRow row = sheet.createRow(rowid++);
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        row.createCell(0).setCellValue("License:");
+        row.createCell(1).setCellValue(l.getName());
+        row.createCell(2).setCellValue("file number:");
+        row.createCell(3).setCellValue(l.getNumber());
+        row = sheet.createRow(rowid++);
+        row.createCell(0).setCellValue("Interval start:");
+        row.createCell(1).setCellValue(df.format(start));
+        row.createCell(2).setCellValue("end:");
+        row.createCell(3).setCellValue(df.format(end));
+        rowid++;
+        row = sheet.createRow(rowid++);
+        row.createCell(0).setCellValue("species");
+        row.createCell(1).setCellValue("no used in reporting period");
+        row.createCell(2).setCellValue("remaining quota");
+        row = sheet.createRow(rowid++);
+        Set<Quota> quotas = l.getQuotas();
+        HashMap<SpeciesType,  Integer> counts = getUsedSpeciesCount(l, start, end);
+        for (Quota q : quotas) {
+            Integer used = counts.containsKey(q.getSpeciesType()) ? counts.get(q.getSpeciesType()) : 0;
+            row.createCell(0).setCellValue(q.getSpeciesType().getName());
+            row.createCell(1).setCellValue(used);
+            row.createCell(2).setCellValue(q.getNumber() - q.getUsed());
+            row = sheet.createRow(rowid++);
+        }
+        row = sheet.createRow(rowid++);
+        row.createCell(0).setCellValue("Treatment");
+        row.createCell(1).setCellValue("Subject");
+        row.createCell(2).setCellValue("Start date");
+        row.createCell(3).setCellValue("End date");
+
+        Set<TreatmentType> types = l.getTreatmentTypes();
+        for (TreatmentType t : types) {
+            rowid = exportTreatmentType(t, sheet, rowid, start, end);
+        }
+    }
+
+
+    private HashMap<SpeciesType, Integer> getUsedSpeciesCount(License l, Date start, Date end) {
+        HashMap<SpeciesType, Integer> counts = new HashMap<>();
+        List<Treatment> treatments = new LinkedList<>();
+        for (TreatmentType tt: l.getTreatmentTypes()) {
+            treatments.addAll(tt.getTreatments());
+        }
+        for (Treatment t : treatments) {
+            if (t.getStart().before(end) && (t.getEnd() != null && t.getEnd().before(end) && t.getEnd().after(start))) {
+                SpeciesType st = t.getSubject().getSpeciesType();
+                if (counts.containsKey(st)) {
+                    counts.put(st, counts.get(st) + 1);
+                } else {
+                    counts.put(st, 1);
+                }
+            }
+        }
+        return counts;
+    }
+
+
+    public int exportTreatmentType(TreatmentType t, XSSFSheet sheet, int rowid, Date start, Date end) {
+        Set<Treatment> treatments = t.getTreatments();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+        if(treatments.size() == 0) {
+            return rowid;
+        }
+        XSSFRow row = sheet.createRow(rowid++);
+        row.createCell(0).setCellValue(t.getName());
+        for (Treatment treatment : treatments) {
+            if (treatment.getEnd() == null || (treatment.getEnd().before(start) || treatment.getEnd().after(end))) {
+                continue;
+            }
+            row.createCell(1).setCellValue(treatment.getSubject().getName());
+            row.createCell(2).setCellValue(sdf.format(treatment.getStart()));
+            row.createCell(3).setCellValue(sdf.format(treatment.getEnd()));
+            row = sheet.createRow(rowid++);
+        }
+        return rowid;
+    }
 }
+
+
