53956e18e0500ae9a0dcea151f1e2c6e063a85a0
DennisHub <dennis.huben@rwth-aachen.de>
2017-03-07 11:07:31 +0100

    Added migration file to extend changelog

---
+++ migrations/0005-extendchangelog.sql
@@ -0,0 +1,6 @@
+-- -----------------------------------------------------
+-- Table `animal_keeping`.`changelog`
+-- -----------------------------------------------------
+
+ALTER TABLE ` animal_keeping`.`changelog` ADD `who` VARCHAR(200) NOT NULL;
+
