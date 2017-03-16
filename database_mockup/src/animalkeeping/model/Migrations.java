aeba8ef08dd38a31620277d0b72db044e944f8eb
Jan Grewe <jan.grewe@g-node.org>
2017-03-16 22:58:43 +0100

    [Migration] mapping and class for Migrations

---
+++ database_mockup/src/animalkeeping/model/Migrations.java
@@ -0,0 +1,36 @@
+package animalkeeping.model;
+
+import java.util.Date;
+
+/**
+ * Created by jan on 16.03.17.
+ */
+public class Migrations {
+    private long id;
+    private String name;
+    private java.util.Date migrationDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getMigrationDate() {
+        return migrationDate;
+    }
+
+    public void setMigrationDate(Date migrationDate) {
+        this.migrationDate = migrationDate;
+    }
+}
