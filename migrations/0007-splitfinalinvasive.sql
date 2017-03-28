702436dc52a8a4722568a51c0568f56748f3c000
Jan Grewe <jan.grewe@g-node.org>
2017-03-28 22:51:08 +0200

    [migration] add enum column for selection of treatment target

--- migrations/0007-splitfinalinvasive.sql
+++ migrations/0007-splitfinalinvasive.sql
@@ -2,6 +2,7 @@
 -- ALTER table `animalkeeping`.`treatment_type`
 -- -----------------------------------------------------
 ALTER TABLE `animal_keeping`.`census_treatmenttype` ADD COLUMN `isfinal` tinyint(1) NOT NULL DEFAULT 0 AFTER `invasive`;
+ALTER TABLE `animal_keeping`.`census_treatmenttype` ADD COLUMN `target` ENUM(`subject`, `housing` NOT NULL) DEFAULT `subject`;
 
 UPDATE `animal_keeping`.`census_treatmenttype` SET `isfinal` = 1 where `invasive` = 1;
 
