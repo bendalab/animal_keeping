d9a2dea6ab9cd84304b3b5955a23f504b3e1c8ef
Jan Grewe <jan.grewe@g-node.org>
2017-04-13 12:19:05 +0200

    [HousingTypeDialog] fix resizing issues

--- database_mockup/src/animalkeeping/ui/HousingTypeDialog.java
+++ database_mockup/src/animalkeeping/ui/HousingTypeDialog.java
@@ -5,7 +5,9 @@ import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 
@@ -37,17 +39,28 @@ public class HousingTypeDialog extends VBox {
         descriptionArea.setWrapText(true);
 
         GridPane grid = new GridPane();
+        ColumnConstraints column1 = new ColumnConstraints(100,100, Double.MAX_VALUE);
+        column1.setHgrow(Priority.NEVER);
+        ColumnConstraints column2 = new ColumnConstraints(100, 150, Double.MAX_VALUE);
+        column2.setHgrow(Priority.ALWAYS);
+        grid.getColumnConstraints().addAll(column1, column2);
+
+        idLabel.prefWidthProperty().bind(column2.maxWidthProperty());
+        nameField.prefWidthProperty().bind(column2.maxWidthProperty());
+        descriptionArea.prefWidthProperty().bind(column2.maxWidthProperty());
+
         grid.setPadding(new Insets(5, 0, 5, 0));
-        grid.add(new Label("ID"), 1, 1);
-        grid.add(idLabel, 2, 1);
+        grid.add(new Label("ID"), 0, 0);
+        grid.add(idLabel, 1, 0);
 
-        grid.add(new Label("name (*)"), 1, 2);
-        grid.add(nameField, 2, 2, 2,1);
+        grid.add(new Label("name (*)"), 0, 1);
+        grid.add(nameField, 1, 1, 2,1);
 
-        grid.add(new Label("description"), 1, 3);
-        grid.add(descriptionArea, 1, 4, 3, 3);
+        grid.add(new Label("description"), 0, 2);
+        grid.add(descriptionArea, 0, 3, 3, 3);
 
-        grid.add(new Label("(*) required"), 1, 7);
+        grid.add(new Label("(*) required"), 0, 6);
+
         this.getChildren().add(grid);
     }
 
