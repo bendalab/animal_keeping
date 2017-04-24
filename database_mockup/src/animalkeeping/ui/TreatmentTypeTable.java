3c2686c7af0ec166277cb2e34093aa1caf1fc26b
Jan Grewe <jan.grewe@g-node.org>
2017-04-24 23:22:47 +0200

    [TreatmentTypeTable] refresh with thread

--- database_mockup/src/animalkeeping/ui/TreatmentTypeTable.java
+++ database_mockup/src/animalkeeping/ui/TreatmentTypeTable.java
@@ -3,6 +3,7 @@ package animalkeeping.ui;
 import animalkeeping.model.TreatmentType;
 import animalkeeping.util.Dialogs;
 import animalkeeping.util.EntityHelper;
+import javafx.application.Platform;
 import javafx.beans.property.ReadOnlyBooleanWrapper;
 import javafx.beans.property.ReadOnlyLongWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
@@ -10,6 +11,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
@@ -22,46 +27,38 @@ import java.util.List;
  * Created by jan on 19.02.17.
  */
 public class TreatmentTypeTable extends TableView<TreatmentType> {
-    private TableColumn<TreatmentType, Number> idCol;
-    private TableColumn<TreatmentType, String> nameCol;
-    private TableColumn<TreatmentType, Boolean> invasiveCol;
-    private TableColumn<TreatmentType, Boolean> finalCol;
-    private TableColumn<TreatmentType, String> targetCol;
-    private TableColumn<TreatmentType, String> licenseCol;
-    private TableColumn<TreatmentType, String> descriptionCol;
-    private ObservableList<TreatmentType> masterList = FXCollections.observableArrayList();
-    private FilteredList<TreatmentType> filteredList;
 
 
     public TreatmentTypeTable() {
         super();
-        idCol = new TableColumn<>("id");
+        this.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        TableColumn<TreatmentType, Number> idCol = new TableColumn<>("id");
         idCol.setCellValueFactory(data -> new ReadOnlyLongWrapper(data.getValue().getId()));
         idCol.prefWidthProperty().bind(this.widthProperty().multiply(0.04));
 
-        nameCol = new TableColumn<>("name");
+        TableColumn<TreatmentType, String> nameCol = new TableColumn<>("name");
         nameCol.setCellValueFactory(data -> new ReadOnlyStringWrapper(data.getValue().getName()));
         nameCol.prefWidthProperty().bind(this.widthProperty().multiply(0.20));
 
-        licenseCol = new TableColumn<>("license");
+        TableColumn<TreatmentType, String> licenseCol = new TableColumn<>("license");
         licenseCol.setCellValueFactory(data -> new ReadOnlyStringWrapper(data.getValue().getLicense() != null ? data.getValue().getLicense().getName() : ""));
         licenseCol.prefWidthProperty().bind(this.widthProperty().multiply(0.20));
 
-        invasiveCol = new TableColumn<>("invasive");
+        TableColumn<TreatmentType, Boolean> invasiveCol = new TableColumn<>("invasive");
         invasiveCol.setCellValueFactory(data -> new ReadOnlyBooleanWrapper(data.getValue().isInvasive()));
-        invasiveCol.setCellFactory( tc -> new CheckBoxTableCell<>());
+        invasiveCol.setCellFactory(tc -> new CheckBoxTableCell<>());
         invasiveCol.prefWidthProperty().bind(this.widthProperty().multiply(0.07));
 
-        finalCol = new TableColumn<>("final");
+        TableColumn<TreatmentType, Boolean> finalCol = new TableColumn<>("final");
         finalCol.setCellValueFactory(data -> new ReadOnlyBooleanWrapper(data.getValue().isFinalExperiment()));
         finalCol.prefWidthProperty().bind(this.widthProperty().multiply(0.07));
-        finalCol.setCellFactory( tc -> new CheckBoxTableCell<>());
+        finalCol.setCellFactory(tc -> new CheckBoxTableCell<>());
 
-        targetCol = new TableColumn<>("target");
+        TableColumn<TreatmentType, String> targetCol = new TableColumn<>("target");
         targetCol.setCellValueFactory(data -> new ReadOnlyStringWrapper(data.getValue().getTarget().toString()));
         targetCol.prefWidthProperty().bind(this.widthProperty().multiply(0.1));
 
-        descriptionCol = new TableColumn<>("description");
+        TableColumn<TreatmentType, String> descriptionCol = new TableColumn<>("description");
         descriptionCol.setCellValueFactory(data -> new ReadOnlyStringWrapper(data.getValue().getDescription() != null ?
                 data.getValue().getDescription() : ""));
         descriptionCol.prefWidthProperty().bind(this.widthProperty().multiply(0.33));
@@ -75,14 +72,12 @@ public class TreatmentTypeTable extends TableView<TreatmentType> {
                     tt = Dialogs.editTreatmentTypeDialog(tt);
                     if (tt != null) {
                         refresh();
-                        setSelectedTreatmentType(tt);
+                        getSelectionModel().select(tt);
                     }
                 }
             });
             return row ;
         });
-
-        init();
     }
 
 
@@ -93,28 +88,40 @@ public class TreatmentTypeTable extends TableView<TreatmentType> {
 
 
     private void init() {
-        List<TreatmentType> result = EntityHelper.getEntityList("from TreatmentType", TreatmentType.class);
-        masterList.addAll(result);
-        filteredList = new FilteredList<>(masterList, p -> true);
-        SortedList<TreatmentType> sortedList = new SortedList<>(filteredList);
-        sortedList.comparatorProperty().bind(this.comparatorProperty());
-        this.setItems(sortedList);
+        TreatmentType t  = getSelectionModel().getSelectedItem();
+        Task<Void> refreshTask = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                List<TreatmentType> tts = EntityHelper.getEntityList("from TreatmentType", TreatmentType.class);
+
+                Platform.runLater(() -> {
+                    getItems().clear();
+                    getItems().addAll(tts);
+                    getSelectionModel().select(t);
+                });
+                return null;
+            }
+        };
+        new Thread(refreshTask).start();
     }
 
     public void setTreatmentTypes(Collection<TreatmentType> types) {
-        this.masterList.clear();
-        this.masterList.addAll(types);
+        this.getItems().clear();
+        this.getItems().addAll(types);
     }
 
     @Override
     public void refresh() {
-        masterList.clear();
         init();
         super.refresh();
     }
 
+    /*
     public void setSelectedTreatmentType(TreatmentType treatmentType) {
-        this.getSelectionModel().select(treatmentType);
+        System.out.println(this.getItems().contains(treatmentType));
+        int index = getItems().indexOf(treatmentType);
+        this.getSelectionModel().select(index);
     }
+    */
 }
 
