57726464b8a5da4e3680a4b0c090ceddf629332b
Jan Grewe <jan.grewe@g-node.org>
2017-06-14 16:29:01 +0200

    [views] add custom ToolTips

--- database_mockup/src/animalkeeping/ui/views/InventoryController.java
+++ database_mockup/src/animalkeeping/ui/views/InventoryController.java
@@ -1,3 +1,38 @@
+/******************************************************************************
+ Copyright (c) 2017 Neuroethology Lab, University of Tuebingen,
+ Jan Grewe <jan.grewe@g-node.org>,
+ Dennis Huben <dennis.huben@rwth-aachen.de>
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this list
+ of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice, this
+ list of conditions and the following disclaimer in the documentation and/or other
+ materials provided with the distribution.
+
+ 3. Neither the name of the copyright holder nor the names of its contributors may
+ be used to endorse or promote products derived from this software without specific
+ prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ DAMAGE.
+
+ * Created by jan on 14.01.17.
+
+ *****************************************************************************/
 package animalkeeping.ui.views;
 
 import animalkeeping.model.*;
@@ -40,9 +75,6 @@ import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-/**
- * Created by jan on 14.01.17.
- */
 public class InventoryController extends AbstractView implements Initializable {
     @FXML private PieChart populationChart;
     @FXML private VBox unitsBox;
@@ -509,6 +541,10 @@ public class InventoryController extends AbstractView implements Initializable {
 
     private void treatmentSelected(Treatment t) {
         endTreatmentLabel.setDisable(t == null);
+    }
+
+    public static Tooltip getToolTip() {
+        return new Tooltip("Gives an overview over the animal population. Offers export functions.");
     }
 }
 
