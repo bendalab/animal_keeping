bb1e90c618d07a24c7733ef615107d0ef9492136
Jan Grewe <jan.grewe@g-node.org>
2017-06-21 15:18:21 +0200

    [HousingView] fix refresh performance issue

--- database_mockup/src/animalkeeping/ui/tables/HousingUnitTable.java
+++ database_mockup/src/animalkeeping/ui/tables/HousingUnitTable.java
@@ -1,28 +1,64 @@
+/******************************************************************************
+ Copyright (c) 2017 Neuroethology Lab, University of Tuebingen,
+ Jan Grewe <jan.grewe@g-node.org>,
+ Dennis Huben <dennis.huben@rwth-aachen.de>
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this list
+ of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice, this
+ list of conditions and the following disclaimer in the documentation and/or other
+ materials provided with the distribution.
+
+ 3. Neither the name of the copyright holder nor the names of its contributors may
+ be used to endorse or promote products derived from this software without specific
+ prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ DAMAGE.
+
+ * Created by jan on 01.05.17.
+
+ *****************************************************************************/
 package animalkeeping.ui.tables;
 
 import animalkeeping.logging.Communicator;
 import animalkeeping.model.HousingUnit;
+import animalkeeping.ui.views.ViewEvent;
 import animalkeeping.util.Dialogs;
 import animalkeeping.util.EntityHelper;
-import javafx.application.Platform;
 import javafx.beans.property.ReadOnlyIntegerWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ListChangeListener;
 import javafx.concurrent.Task;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Callback;
 
 import java.util.HashMap;
 import java.util.List;
 
 import static animalkeeping.util.Dialogs.showInfo;
 
-/**
- * Created by grewe on 2/14/17.
- */
 public class HousingUnitTable extends TreeTableView<HousingUnit> {
     private MenuItem editItem, deleteItem, appendItem;
-
+    private TreeItem<HousingUnit> root = new TreeItem<>();
 
     public HousingUnitTable () {
         //super();
@@ -31,29 +67,27 @@ public class HousingUnitTable extends TreeTableView<HousingUnit> {
 
     public void initialize() {
         TreeTableColumn<HousingUnit, String> unitsColumn = new TreeTableColumn<>("housing unit");
-        unitsColumn.setCellValueFactory((TreeTableColumn.CellDataFeatures<HousingUnit, String> hu) ->
-                new ReadOnlyStringWrapper(hu.getValue().getValue() != null ? hu.getValue().getValue().getName() : ""));
+        unitsColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("Name"));
         unitsColumn.prefWidthProperty().bind(this.widthProperty().multiply(0.25));
 
         TreeTableColumn<HousingUnit, String> typeColumn = new TreeTableColumn<>("type");
+        typeColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("HousingType"));
         typeColumn.setCellValueFactory((TreeTableColumn.CellDataFeatures<HousingUnit, String> hu) ->
                 new ReadOnlyStringWrapper(hu.getValue().getValue() != null ? hu.getValue().getValue().getHousingType().getName() : ""));
         typeColumn.prefWidthProperty().bind(this.widthProperty().multiply(0.2));
 
         TreeTableColumn<HousingUnit, String> dimensionColumn = new TreeTableColumn<>("dimension");
-        dimensionColumn.setCellValueFactory((TreeTableColumn.CellDataFeatures<HousingUnit, String> hu) ->
-                new ReadOnlyStringWrapper(hu.getValue().getValue() != null ? hu.getValue().getValue().getDimensions() : ""));
+        dimensionColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("Dimensions"));
         dimensionColumn.prefWidthProperty().bind(this.widthProperty().multiply(0.15));
 
         TreeTableColumn<HousingUnit, String> descriptionColumn = new TreeTableColumn<>("description");
-        descriptionColumn.setCellValueFactory((TreeTableColumn.CellDataFeatures<HousingUnit, String> hu) ->
-                new ReadOnlyStringWrapper(hu.getValue().getValue() != null ? hu.getValue().getValue().getDescription() : ""));
+        descriptionColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("Description"));
         descriptionColumn.prefWidthProperty().bind(this.widthProperty().multiply(0.25));
 
         TreeTableColumn<HousingUnit, Number> populationColumn = new TreeTableColumn<>("population");
-        populationColumn.setCellValueFactory((TreeTableColumn.CellDataFeatures<HousingUnit, Number> hu) ->
-                new ReadOnlyIntegerWrapper(hu.getValue().getValue() != null ? hu.getValue().getValue().getAllHousings(true).size() : 0));
+        populationColumn.setCellValueFactory(new TreeItemPropertyValueFactory<>("AllHousings"));
         populationColumn.prefWidthProperty().bind(this.widthProperty().multiply(0.15));
+
         this.getColumns().addAll(unitsColumn, typeColumn, dimensionColumn, descriptionColumn, populationColumn);
         this.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         this.addEventFilter(MouseEvent.MOUSE_CLICKED, event -> {
@@ -92,12 +126,33 @@ public class HousingUnitTable extends TreeTableView<HousingUnit> {
 
         cmenu.getItems().addAll(newItem, appendItem, editItem, deleteItem);
         setContextMenu(cmenu);
+        this.setRoot(root);
+        this.setShowRoot(false);
     }
 
     @Override
     public void refresh() {
-        fillHousingTree();
-        super.refresh();
+        root.getChildren().clear();
+        HashMap<String, Boolean> folding = getFoldingState();
+        TreeItem<HousingUnit> seletedItem = getSelectionModel().getSelectedItem();
+        Task<Void> refresh_task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                List<HousingUnit> housingUnits = EntityHelper.getEntityList("from HousingUnit where parentUnit is null", HousingUnit.class);
+                for (HousingUnit hu : housingUnits) {
+                    TreeItem<HousingUnit> child = new TreeItem<>(hu);
+                    root.getChildren().add(child);
+                    fillRecursive(hu, child);
+                }
+                return  null;
+            }
+        };
+        refresh_task.setOnSucceeded(event -> {
+            setFoldingState(folding);
+            getSelectionModel().select(seletedItem);
+            fireEvent(new ViewEvent(ViewEvent.REFRESHED));
+        });
+        new Thread(refresh_task).run();
     }
 
     private HashMap<String, Boolean> getFoldingState() {
@@ -129,33 +184,6 @@ public class HousingUnitTable extends TreeTableView<HousingUnit> {
             }
             row++;
         }
-    }
-
-
-    private void fillHousingTree() {
-        Task<Void> refreshTask = new Task<Void>() {
-            @Override
-            protected Void call() throws Exception {
-            HashMap<String, Boolean> folding = getFoldingState();
-            TreeItem<HousingUnit> seletedItem = getSelectionModel().getSelectedItem();
-            final TreeItem<HousingUnit> root = new TreeItem<>();
-            List<HousingUnit> housingUnits = EntityHelper.getEntityList("from HousingUnit where parentUnit is null", HousingUnit.class);
-            for (HousingUnit hu : housingUnits) {
-                TreeItem<HousingUnit> child = new TreeItem<>(hu);
-                root.getChildren().add(child);
-                fillRecursive(hu, child);
-            }
-            Platform.runLater(() -> {
-                root.setExpanded(true);
-                setRoot(root);
-                setShowRoot(false);
-                setFoldingState(folding);
-                getSelectionModel().select(seletedItem);
-            });
-            return null;
-            }
-        };
-        new Thread(refreshTask).start();
     }
 
 
