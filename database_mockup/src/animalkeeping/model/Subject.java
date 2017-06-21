7a12d560d41eb7101e92131ce6b9146c04117532
Jan Grewe <jan.grewe@g-node.org>
2017-06-21 22:49:16 +0200

    [subject] add property returning the exitDate

--- database_mockup/src/animalkeeping/model/Subject.java
+++ database_mockup/src/animalkeeping/model/Subject.java
@@ -53,6 +53,7 @@ public class Subject extends Entity implements ChangeLogInterface {
     private SubjectType subjectType;
     private SupplierType supplier;
     private Date importDate;
+    private Date exitDate;
     private Set<Treatment> treatments =
             new HashSet<Treatment>(0);
     private Set<SubjectNote> notes =
@@ -156,6 +157,14 @@ public class Subject extends Entity implements ChangeLogInterface {
 
     public void setImportDate(Date importDate) {
         this.importDate = importDate;
+    }
+
+    public Date getExitDate() {
+        return exitDate;
+    }
+
+    public void setExitDate(Date exitDate) {
+        this.exitDate = exitDate;
     }
 
     public Set<SubjectNote> getNotes() {
