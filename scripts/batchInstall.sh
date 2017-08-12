4b6f53da621610c51948089dd6785c6bbec5757e
Jan Grewe <jan.grewe@g-node.org>
2017-08-12 15:02:59 +0200

    [scripts] make install script la little more generic

--- scripts/batchInstall.sh
+++ scripts/batchInstall.sh
@@ -17,16 +17,12 @@ read -s -p "Please enter db user password:  `echo $'\n> '`" db_user_password
 mysql --user=$db_admin_name --password=$db_admin_password --execute="CREATE DATABASE 	$db_name"
 mysql --user=$db_admin_name --password=$db_admin_password --execute="GRANT ALL ON 	$db_name.* TO '$db_admin_name'@'localhost' IDENTIFIED BY '$db_admin_password'"
 
-mysql $db_name < ../migrations/0001-initial.sql 		--host=localhost --user=$db_admin_name --password=$db_admin_password
-mysql $db_name < ../migrations/0002-changelog.sql 		--host=localhost --user=$db_admin_name --password=$db_admin_password
-mysql $db_name < ../migrations/0003-databaseuser.sql 		--host=localhost --user=$db_admin_name --password=$db_admin_password
-mysql $db_name < ../migrations/0004-linkuserandperson.sql	--host=localhost --user=$db_admin_name --password=$db_admin_password
-mysql $db_name < ../migrations/0005-extendchangelog.sql 	--host=localhost --user=$db_admin_name --password=$db_admin_password
-mysql $db_name < ../migrations/0006-migrationlog.sql 		--host=localhost --user=$db_admin_name --password=$db_admin_password
-mysql $db_name < ../migrations/0007-splitfinalinvasive.sql 	--host=localhost --user=$db_admin_name --password=$db_admin_password
-mysql $db_name < ../migrations/0008-licenseinfo.sql  		--host=localhost --user=$db_admin_name --password=$db_admin_password
-mysql $db_name < ../migrations/0009-subjectgender.sql 		--host=localhost --user=$db_admin_name --password=$db_admin_password
-mysql $db_name < ../migrations/0010-changelogquery.sql 		--host=localhost --user=$db_admin_name --password=$db_admin_password
+migrations = 'ls ../migrations/*.sql'
+
+for script in "${migrations[@]}"; do
+    echo "$script"
+    mysql $db_name < $script --host=localhost --user=$db_admin_name --password=$db_admin_password
+done
 
 mysql --user=$db_admin_name --password=$db_admin_password --execute="CREATE USER '$db_user_name'@'localhost' IDENTIFIED BY '$db_user_password'"
 mysql --user=$db_admin_name --password=$db_admin_password --execute="CREATE USER '$db_user_name'@'%' IDENTIFIED BY '$db_user_password'"
