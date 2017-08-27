25bced3b51fd65934b2501619a56082eec197839
Jan Grewe <jan.grewe@g-node.org>
2017-08-27 18:33:36 +0200

    [SubjectForms] show only active persons according to app settings

--- database_mockup/src/animalkeeping/ui/forms/AddSubjectsForm.java
+++ database_mockup/src/animalkeeping/ui/forms/AddSubjectsForm.java
@@ -2,6 +2,7 @@ package animalkeeping.ui.forms;
 
 import animalkeeping.logging.Communicator;
 import animalkeeping.model.*;
+import animalkeeping.ui.Main;
 import animalkeeping.ui.widgets.HousingDropDown;
 import animalkeeping.ui.widgets.SpecialTextField;
 import animalkeeping.util.DateTimeHelper;
@@ -25,6 +26,43 @@ import java.util.List;
 import java.util.prefs.Preferences;
 
 import static animalkeeping.util.Dialogs.editHousingUnitDialog;
+
+/******************************************************************************
+ animalBase
+ animalkeeping.ui.forms
+
+ Copyright (c) 2017 Neuroethology Lab, University of Tuebingen,
+ Jan Grewe <jan.grewe@g-node.org>,
+ Dennis Huben <dennis.huben@rwth-aachen.de>
+
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this list
+ of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice, this
+ list of conditions and the following disclaimer in the documentation and/or other
+ materials provided with the distribution.
+
+ 3. Neither the name of the copyright holder nor the names of its contributors may
+ be used to endorse or promote products derived from this software without specific
+ prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ DAMAGE.
+
+ *****************************************************************************/
 
 
 public class AddSubjectsForm extends VBox {
@@ -128,7 +166,13 @@ public class AddSubjectsForm extends VBox {
         List<SupplierType> supplier = EntityHelper.getEntityList("from SupplierType", SupplierType.class);
         List<SpeciesType> species = EntityHelper.getEntityList("from SpeciesType", SpeciesType.class);
         List<SubjectType> subjectTypes = EntityHelper.getEntityList("from SubjectType where name = 'animal'", SubjectType.class);
-        List<Person> persons = EntityHelper.getEntityList("from Person", Person.class);
+
+        List<Person> persons;
+        if (Main.getSettings().isActivePersonsSelection()) {
+            persons = EntityHelper.getEntityList("from Person where active = True", Person.class);
+        } else {
+            persons = EntityHelper.getEntityList("from Person", Person.class);
+        }
 
         st = subjectTypes.get(0);
         supplierComboBox.getItems().addAll(supplier);
